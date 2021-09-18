a50f844a52fdf1ee52be4f40fb53ecc8fc242e2c
XGalilei <ianbush1@gmail.com>
2021-09-18 14:37:27 -0500

    Rudimentary button set-up ready

--- README.md
+++ README.md
@@ -7,6 +7,10 @@
 
 A gallery of items relevant to my life, built in React. 
 
-Your project description goes here. What problem did you solve? How did you solve it?
-
-Additional README details can be found [here](https://github.com/PrimeAcademy/readme-template/blob/master/README.md).
+### To-Do List
+- [] Use `axios` to retrieve data from the gallery module and store it in `App.jsx`
+- [] Create a `GalleryList` component to store all gallery data
+- [] Create a `GalleryItem` component to store a single instance of an image in a gallery
+- - [] Should contain the number of likes, a like button, an image, and a description
+- - [] Image should swap places with description when clicked
+- []
--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,16 +1,33 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Gallery of My Life</h1>
-        </header>
-        <p>Gallery goes here</p>
-        <img src="images/goat_small.jpg"/>
+  //holds the items in the gallery
+  const [galleryList, setGalleryList] = useState([]);
+  const [description, setDescription] = useState();
+  let [imageLikes, setImageLikes] = useState(1);
+
+  const likeImage = ()=> {
+    setImageLikes(imageLikes + 1);
+    console.log(imageLikes);
+  }
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="App-title">Gallery of My Life</h1>
+      </header>
+      <p>Gallery goes here</p>
+      <div>
+        <img onClick={() => console.log("testing")} src="images/goat_small.jpg" />
+        <br/>
+        <button onClick= {()=> likeImage()}>Like Button</button>
+        <p>Nobody likes me, everybody hates me, I guess I'll go eat worms!</p>
       </div>
-    );
+      
+    </div>
+  );
 }
 
 export default App;
